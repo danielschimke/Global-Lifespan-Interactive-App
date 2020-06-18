1f29a69832bd6e0273e7dab2168c92795a64c2f4
Daniel Schimke <danieljschimke@gmail.com>
2020-06-18 13:07:18 -0500

    Added a page to add a new entry to the database

--- server.R
+++ server.R
@@ -7,7 +7,12 @@ library(ggplot2)
 
 server <- function(input, output) {
   
-  lifeData <- data.frame(read.csv("life-expectancy.csv"))
+#  lifeData <- data.frame(read.csv("life-expectancy.csv"))
+  dataAccess <- "data.db"
+  con <- dbConnect(drv = RSQLite::SQLite(), dbname = dataAccess)
+  lifeData <- dbReadTable(con, "Merged Table")
+  dbDisconnect(con)
+  
   #Display simple life expectancy data
   newLifeData <- reactive({
     req(input$updateLifeChart)
@@ -15,6 +20,23 @@ server <- function(input, output) {
   })
   output$lifeChart <- renderPlot({
     ggplot(newLifeData()) +
-      geom_line(aes(x=Year, y=Life.expectancy..years., color=Entity))
+      geom_line(aes(x=Year, y=lifeExpectancy, color=Entity))
+  })
+  
+  #Update the database with a new entry
+  observeEvent(input$submitNewEntry, {
+    if(as.numeric(input$newExpectancy) >= 0 && as.numeric(input$newExpectancy) <= 124){
+      output$submitMessage <- renderText({"Submission added"})
+      lifeData <- add_row(lifeData, Entity = input$newEntity, Year = as.numeric(input$newYear), Code = input$newCode,
+              lifeExpectancy = as.numeric(input$newExpectancy), childMortality = as.numeric(input$newChildMortality))
+      
+      dataUpdate <- "data.db"
+      con <- dbConnect(drv = RSQLite::SQLite(), dbname = dataUpdate)
+      dbWriteTable(con, "Merged Table", lifeData, overwrite = TRUE)
+      dbDisconnect(con)
+    }else{
+      output$submitMessage <- renderText({"Error: Enter a valid life expectancy"})
+    }
+    
   })
 }
--- ui.R
+++ ui.R
@@ -3,12 +3,21 @@ library(shinydashboard)
 library(datasets)
 library(tidyverse)
 library(ggplot2)
+library(DBI)
 
+#lifeData <- data.frame(read.csv("life-expectancy.csv"))
+
+dataAccess <- "data.db"
+  con <- dbConnect(drv = RSQLite::SQLite(), dbname = dataAccess)
+  lifeData <- dbReadTable(con, "Merged Table")
+  dbDisconnect(con)
+  
 ui <- dashboardPage(
     dashboardHeader(title = "Fun with databases"),
     dashboardSidebar(
       sidebarMenu(
-        menuItem("Life Expectancy", tabName = "life", icon = icon("heartbeat"))
+        menuItem("Life Expectancy", tabName = "life", icon = icon("heartbeat")),
+        menuItem("Add Entry", tabName = "newEntry", icon = icon("file"))
       )
     ),
     dashboardBody(
@@ -19,6 +28,16 @@ ui <- dashboardPage(
                              choices = unique(lifeData$Entity),
                              selected = NULL, inline = TRUE),
           plotOutput("lifeChart")        
+        ),
+        tabItem(tabName = "newEntry",
+          h3("Add a new entry to the database"),
+          selectInput("newEntity", label = "Entity:", choices = unique(lifeData$Entity)),
+          textInput("newCode", label = "Code:", placeholder = "ex. AFG"),
+          textInput("newYear", label = "Year:", placeholder = "####"),
+          textInput("newExpectancy", label = "Life Expectancy:", placeholder = "Enter a number in years..."),
+          textInput("newChildMortality", label = "Child Mortality Rate:", placeholder = "Ex. 56 for 56%"),
+          actionButton("submitNewEntry", label = "Submit"),
+          textOutput("submitMessage")
         )
       )
     )
